dcc1b53a9e6d3c23bfad3c6d5ffd04219ab52254
Nghia Nguyen <comictvn@gmail.com>
2017-02-02 20:37:41 +0700

    fix

--- lib/purging/images.rb
+++ lib/purging/images.rb
@@ -1,24 +1,28 @@
 require "purging/images/version"
 
-class Purging::Images
-  include HTTParty
-  API_KEY = ENV.fetch('IMGIX_API_KEY'.freeze)
-  base_uri 'https://api.imgix.com/v2'.freeze
+module Purging
+  module Images
+    class Cache
+      include HTTParty
+      API_KEY = ENV.fetch('IMGIX_API_KEY'.freeze)
+      base_uri 'https://api.imgix.com/v2'.freeze
 
-  class << self
-    def call(url)
-      new(url).call
+      class << self
+        def call(url)
+          new(url).call
+        end
+      end
+
+      def initialize(url)
+        @options = { body: { url: url }, basic_auth: { username: API_KEY, password: '' } }
+      end
+
+      def call
+        purge = self.class.post('/image/purger'.freeze, @options)
+        purge.success?
+      rescue Errno::ECONNRESET, SocketError
+        { 'status' => 'failed' }
+      end
     end
-  end
-
-  def initialize(url)
-    @options = { body: { url: url }, basic_auth: { username: API_KEY, password: '' } }
-  end
-
-  def call
-    purge = self.class.post('/image/purger'.freeze, @options)
-    purge.success?
-  rescue Errno::ECONNRESET, SocketError
-    { 'status' => 'failed' }
   end
 end
